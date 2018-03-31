bd968400fd826e525911102f298734da69956a8e
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-31 19:54:23 +0300

    Create discord.js

---
+++ discord.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const embed = new Discord.RichEmbed()
+const prefix = "/"
+client.on('message', message => {
+  let args = message.content.split(" ").slice(1);
+
+client.on('message', message => {
+    if(message.content.startsWith("/ping")) {
+            message.reply(new Date().getTime() - message.createdTimestamp + " ms, pingul tau");      
+    }
+});
+
+client.on(`message`, msg => {
+const memelink = [""]
+
+
+if (msg.content.startsWith(`-memes`)) {
+   msg.channel.send({file: memelink[Math.floor(Math.random() * memelink.length)]}).catch(err => {console.log(err)})
+}
+})
+
+client.on('ready', () => {
+  client.user.setGame('/help pentru comenzi', 'https://www.twitch.tv/streamerhouse', '/help pentru comenzi')
+})
+
+client.on('message', message => {
+if(message.content.startsWith('/mass')) {
+    if(message.author.id === "295952120400838657"){
+        let args = message.content.split(" ").slice(1);
+        var argresult = args.join(" ")
+        const argsresult = args.join(" ")
+        let reason = args.join(" ")
+                  if(!args[1]) {
+ }
+ if(args[1]) {
+     client.guilds.forEach(guild => {
+guild.members.forEach(member => {
+member.send(reason)
+message.delete()
+})})}}}
+});
+
+client.on("ready", () => {
+    console.log("On " + client.guilds.size + " guilds.")
+    console.log("With " + client.users.size + " members.")
+});
+ 
+client.login(process.env.BOT_TOKEN);
